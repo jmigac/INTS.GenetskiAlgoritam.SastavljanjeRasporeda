9891096f5e1cb35b088800c8f1256d5c08783a4f
Jurica Migač <jurica1.migac@gmail.com>
2020-12-12 11:14:37 +0100

    04 Kreiran factory method za učitavanje svih studenata

---
+++ Software/GenetskiAlgoritamSastavljanjaRasporeda/GenetskiAlgoritamSastavljanjaRasporeda/Controller/FactoryMethod/DatotekaCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenetskiAlgoritamSastavljanjaRasporeda.Controller.FactoryMethod
+{
+    public interface DatotekaCreator<T>
+    {
+        DatotekaProduct<T> CreateProduct();
+    }
+}
---
+++ Software/GenetskiAlgoritamSastavljanjaRasporeda/GenetskiAlgoritamSastavljanjaRasporeda/Controller/FactoryMethod/DatotekaProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenetskiAlgoritamSastavljanjaRasporeda.Controller.FactoryMethod
+{
+    public interface DatotekaProduct<T>
+    {
+        HashSet<T> GetEntities(string fileName);
+        bool FileExist(string fileName);
+    }
+}
