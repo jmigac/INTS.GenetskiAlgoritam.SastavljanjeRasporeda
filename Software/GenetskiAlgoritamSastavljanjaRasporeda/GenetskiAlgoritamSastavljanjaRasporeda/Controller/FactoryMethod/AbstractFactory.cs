eb9b4153a6a5ae3fe43cc6b2e88a6034f6e10930
Jurica Migač <jurica1.migac@gmail.com>
2020-12-12 11:15:42 +0100

    06 Kreirana klasa za pozivanje sadržaja određene datoteke

---
+++ Software/GenetskiAlgoritamSastavljanjaRasporeda/GenetskiAlgoritamSastavljanjaRasporeda/Controller/FactoryMethod/AbstractFactory.cs
@@ -0,0 +1,24 @@
+﻿using GenetskiAlgoritamSastavljanjaRasporeda.Controller.FactoryMethod.ConcreteCreator;
+using GenetskiAlgoritamSastavljanjaRasporeda.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GenetskiAlgoritamSastavljanjaRasporeda.Controller.FactoryMethod
+{
+    public class AbstractFactory
+    {
+        public static HashSet<Student> GetValuesForStudents(string fileName)
+        {
+            HashSet<Student> entities = new HashSet<Student>();
+            DatotekaCreator<Student> dc =(DatotekaCreator<Student>) new StudentConcreteCreator();
+            DatotekaProduct<Student> dp = dc.CreateProduct();
+            entities = dp.GetEntities(fileName);
+            return entities;
+        }
+    }
+
+}
