42bd3029f150da97308cebd602c6f705455697c6
Jurica Migač <jurica1.migac@gmail.com>
2020-12-28 16:47:26 +0100

    09 Kreirana entitetska klasa za dvoranu

---
+++ Software/GenetskiAlgoritamSastavljanjaRasporeda/GenetskiAlgoritamSastavljanjaRasporeda/Model/Dvorana.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenetskiAlgoritamSastavljanjaRasporeda.Model
+{
+    class Dvorana
+    {
+        private int _id { get; set; }
+        private string _naziv { get; set; }
+        private int _kapacitet { get; set; }
+        private string _lokacija { get; set; }
+
+        public int Id { get=>this._id; set=>this._id=value; }
+        public string Naziv { get => this._naziv; set => this._naziv = value; }
+        public int Kapacitet { get=>this._kapacitet; set=>this._kapacitet=value; }
+        public string Lokacija { get=>this._lokacija; set=>this._lokacija=value; }
+
+        public Dvorana()
+        {
+
+        }
+        public Dvorana(int id,string naziv,int kapacitet, string lokacija)
+        {
+            this.Id = id;
+            this.Naziv = naziv;
+            this.Kapacitet = kapacitet;
+            this.Lokacija = lokacija;
+        }
+    }
+}
